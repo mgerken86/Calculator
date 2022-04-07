a3c31f0ada9a74088aa408a5fa8534e80c3f5e1b
Mark Gerken <mgerken86@gmail.com>
2022-04-06 18:43:36 -0600

    touch up css, created js const

---
+++ script.js
@@ -0,0 +1,9 @@
+const previousInputTextElement = document.querySelector('[data-previous-input]');
+const currentOutputTextElement = document.querySelector('[data-current-output]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operandButtons = document.querySelectorAll('[data-operand]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const plusMinusButton = document.querySelectorAll('[data-plus-minus]');
+const percentageButton = document.querySelectorAll('[data-percentage]');
+
